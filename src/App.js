ca621c638ea835b36054199e23b168776e26768e
Shartechs <shartechs@gmail.com>
2023-08-23 00:35:55 +0200

    pushing project v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+import { messages } from "./messages";
 
-function App() {
+const App = () => {
+  const [step, setStep] = useState(1);
+  const handlePrevious = () => {
+    setStep(step - 1);
+  };
+  const handleNext = () => {
+    setStep(step + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step === 1 ? "active" : null}`}>1</div>
+        <div className={`${step === 2 ? "active" : null}`}>2</div>
+        <div className={`${step === 3 ? "active" : null}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          bgColor="#7950f2"
+          textColor="#fff"
+          disabled={step === 1}
+          onClick={handlePrevious}
         >
-          Learn React
-        </a>
-      </header>
+          <span>👈</span> Previous
+        </button>
+
+        <button
+          bgColor="#7950f2"
+          textColor="#fff"
+          disabled={step === 3}
+          onClick={handleNext}
+        >
+          Next <span>👉</span>
+          <span>🤓</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
